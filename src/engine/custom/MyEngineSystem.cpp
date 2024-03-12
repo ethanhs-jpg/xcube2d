6b0315b74c7047b38a31fa9448ca05f509d4a2b8
Ethan Hosford <ethan.hosford03@gmail.com>
2024-03-12 11:18:50 +0000

    V0.1.1 - Began adding function declarations and definitions

--- src/engine/custom/MyEngineSystem.cpp
+++ src/engine/custom/MyEngineSystem.cpp
@@ -1,3 +1,20 @@
-// WORK HERE
+// =====================
+// Custom physics engine
+// =====================
 
 #include "MyEngineSystem.h"
+
+void MyEngineSystem::setGravity(float value, float interval) {
+	// setting value of gravity here
+
+}
+
+void MyEngineSystem::setDrag(float value, float interval) {
+	// setting value of drag here
+
+}
+
+void MyEngineSystem::update() {
+	// updating the physics here
+
+}
--- src/engine/custom/MyEngineSystem.h
+++ src/engine/custom/MyEngineSystem.h
@@ -1,15 +1,27 @@
-// WORK HERE
+// =====================
+// Custom physics engine
+// =====================
 
 #ifndef __MY_ENGINE_H__
 #define __MY_ENGINE_H__
 
 #include "../EngineCommon.h"
+#include <vector> // including vector library for movement
+#include <memory>
+#include "../GameMath.h" // including the game maths header
 
 class MyEngineSystem {
 	friend class XCube2Engine;
 	private:
+		// private vector variables for gravity and drag
+		Vector2f gravity;
+		Vector2f drag;
 
 	public:
+		// public function declarations for setting gravity, drag, and updating
+		void setGravity(float gravityVal, float worldUpdateInterval);
+		void setDrag(float dragVal, float worldUpdateInterval);
+		void update();
 };
 
 #endif
