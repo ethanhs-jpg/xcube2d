0deed6c547823ef6b05ba707038ab62256ea99de
Ethan Hosford <ethan.hosford03@gmail.com>
2024-03-21 15:39:59 +0000

    V0.1.2 - Laid out development plan

--- src/engine/custom/MyEngineSystem.cpp
+++ src/engine/custom/MyEngineSystem.cpp
@@ -4,12 +4,35 @@
 
 #include "MyEngineSystem.h"
 
+/*
+Features to add
+===============
+
+1. Add world bounds that the player object collides with
+2. Integrate text, images, and audio
+	a. Background picture
+	b. Sound effect when hitting objects
+	c. Text popups?
+3. Add gravity at a chosen value (moon gravity?) to make the player fall
+	a. If they press the downwards movement key (D/down arrow), make them fall faster
+	b. Jumping will have to fight against gravity and eventually be overcome before falling down
+4. Add platforms that players can collide with and jump on - perhaps let them jump around the world and collect items
+5. Add acceleration and deceleration when jumping and moving left/right whilst in the air
+
+===============
+*/
+
+void MyEngineSystem::setBounds(float screenWidth, float screenHeight) {
+	// setting the world bounds here
+
+}
+
 void MyEngineSystem::setGravity(float value, float interval) {
 	// setting value of gravity here
 
 }
 
-void MyEngineSystem::setDrag(float value, float interval) {
+void MyEngineSystem::setAcceleration(float value, float interval) {
 	// setting value of drag here
 
 }
--- src/engine/custom/MyEngineSystem.h
+++ src/engine/custom/MyEngineSystem.h
@@ -10,6 +10,24 @@
 #include <memory>
 #include "../GameMath.h" // including the game maths header
 
+/*
+Features to add
+===============
+
+1. Add world bounds that the player object collides with
+2. Integrate text, images, and audio
+	a. Background picture
+	b. Sound effect when hitting objects
+	c. Text popups?
+3. Add gravity at a chosen value (moon gravity?) to make the player fall
+	a. If they press the downwards movement key (D/down arrow), make them fall faster
+	b. Jumping will have to fight against gravity and eventually be overcome before falling down
+4. Add platforms that players can collide with and jump on - perhaps let them jump around the world and collect items
+5. Add acceleration and deceleration when jumping and moving left/right whilst in the air
+
+===============
+*/
+
 class MyEngineSystem {
 	friend class XCube2Engine;
 	private:
@@ -18,9 +36,9 @@ class MyEngineSystem {
 		Vector2f drag;
 
 	public:
-		// public function declarations for setting gravity, drag, and updating
-		void setGravity(float gravityVal, float worldUpdateInterval);
-		void setDrag(float dragVal, float worldUpdateInterval);
+		void setBounds(float width, float height); // setting world bounds for collision
+		void setGravity(float gravityVal, float worldUpdateInterval); // setting value of gravity
+		void setAcceleration(float dragVal, float worldUpdateInterval); // setting value for acceleration/deceleration
 		void update();
 };
 
